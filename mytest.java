e6b50c5c35fa6212103d7d416fd4e88ffe79743b
Wollf98 <57486695+Wollf98@users.noreply.github.com>
2021-01-07 20:35:00 +0800

    Update mytest.java

--- mytest.java
+++ mytest.java
@@ -2,7 +2,7 @@ public class Tester {
   public void addDept() {
     for (int i = 10; i < 110; i++) {
       System.out.println("haha"+i);
-	  System.out.println("xixi"+i);
+	  System.out.println("xixi1"+i);
     }
   }
 }
